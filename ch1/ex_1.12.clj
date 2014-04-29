f18837cbe3f447225d9ff289c8d530216f5669cc
sethdaugherty <daugherty.seth@gmail.com>
2014-04-28 23:11:42 -0700

    Exercise 1.12

---
+++ ch1/ex_1.12.clj
@@ -0,0 +1,35 @@
+; Implementation of recursive procedure to compute rows of Pascal's triangle
+
+; recursive implementation
+(defn pascal [row column]
+  (cond
+    ;(> column row) (throw (Exception. (str "Invalid column " column)))
+    (= row column) 1
+    (= row 1) 1
+    (= column 1) 1
+    :else
+      (+ 
+        (pascal (- row 1) (- column 1)) 
+        (pascal (- row 1) column)))
+)
+
+(print "row 1 column 1: ")
+(println (pascal 1 1))
+(print "row 2 column 1: ")
+(println (pascal 2 1))
+(print "row 2 column 2: ")
+(println (pascal 2 2))
+(print "row 3 column 1: ")
+(println (pascal 3 1))
+(print "row 3 column 2: ")
+(println (pascal 3 2))
+(print "row 3 column 3: ")
+(println (pascal 3 3))
+(print "row 4 column 1: ")
+(println (pascal 4 1))
+(print "row 4 column 2: ")
+(println (pascal 4 2))
+(print "row 4 column 3: ")
+(println (pascal 4 3))
+(print "row 4 column 4: ")
+(println (pascal 4 4))
