c725c98b49b38552b433e1ecd5e3d3714398f2aa
sethdaugherty <daugherty.seth@gmail.com>
2014-05-01 23:24:28 -0700

    Started exercise 1.21

---
+++ ch1/ex_1.21.clj
@@ -0,0 +1,11 @@
+; Simple, inefficient algorithm to find the smallest divisor of a set of numbers
+
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+
+(defn find-divisor [n test-divisor]
+    (cond 
+  )
+
+
+(println (smallest-divisor 199))
