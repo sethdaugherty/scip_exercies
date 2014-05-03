9b551ba37cde515eb850695c05c4f24a00ea1dd3
sethdaugherty <daugherty.seth@gmail.com>
2014-05-02 20:44:50 -0700

    Fixed exercise 1.21

--- ch1/ex_1.21.clj
+++ ch1/ex_1.21.clj
@@ -1,11 +1,15 @@
 ; Simple, inefficient algorithm to find the smallest divisor of a set of numbers
 
+(defn find-divisor [n test-divisor]
+    (cond (> (* test-divisor test-divisor) n) n
+      (= 0 (rem n test-divisor)) test-divisor
+      :else
+        (find-divisor n (+ test-divisor 1)))
+  )
+
 (defn smallest-divisor [n]
   (find-divisor n 2))
 
-(defn find-divisor [n test-divisor]
-    (cond 
-  )
-
-
 (println (smallest-divisor 199))
+(println (smallest-divisor 1999))
+(println (smallest-divisor 19999))
